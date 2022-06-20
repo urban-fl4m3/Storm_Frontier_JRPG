e294351ad1fa45e0d0401aab1350b226f389c0ad
Oleg Solntsev <urban.fl4m3@gmail.com>
2022-06-20 20:30:57 +0300

    [SF-0] Fixed configs, added custom selection rule

---
+++ Assets/Scripts/SF/Battle/TargetSelection/CustomTargetSelectionRule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using SF.Battle.Actors;
+using SF.Common.Actors;
+
+namespace SF.Battle.TargetSelection
+{
+    public class CustomTargetSelectionRule : ITargetSelectionRule
+    {
+        public event Action<IActor> TargetSelected;
+
+        private readonly BattleActor _actingeActor;
+        private readonly CustomTargetSelectionData _data;
+        
+        public CustomTargetSelectionRule(BattleActor actingeActor, CustomTargetSelectionData data)
+        {
+            _actingeActor = actingeActor;
+            _data = data;
+        }
+        
+        public void TrackSelection(IEnumerable<BattleActor> actors)
+        {
+            if (_data.IsInstant)
+            {
+                if (_data.SelfSelect)
+                {
+                    TargetSelected?.Invoke(_actingeActor);
+                    return;
+                }
+
+                if (_data.SelectAll)
+                {
+                    
+                }
+            }
+        }
+    }
+}
